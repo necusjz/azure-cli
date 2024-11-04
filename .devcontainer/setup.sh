3cccda4808b70e81de7ceb0de772fd8c4150ba68
necusjz <necusjz@gmail.com>
2024-11-04 16:17:58 +0800

    chore: set workspace folder

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # clone required repositories
-git clone https://github.com/Azure/azure-cli-extensions.git /workspaces/azure-cli-extensions
-git clone https://github.com/Azure/azure-rest-api-specs.git /workspaces/azure-rest-api-specs
-git clone https://github.com/Azure/aaz.git /workspaces/aaz
+git clone https://github.com/Azure/azure-cli-extensions.git ./azure-cli-extensions
+git clone https://github.com/Azure/azure-rest-api-specs.git ./azure-rest-api-specs
+git clone https://github.com/Azure/aaz.git ./aaz
 
 # setup development environment
-cd /workspaces
 python -m venv ./venv && source ./venv/bin/activate
 pip install aaz-dev
 azdev setup -c -r ./azure-cli-extensions
