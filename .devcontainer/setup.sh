c86c76316d549c00af146644e04224537b4bae6e
necusjz <necusjz@gmail.com>
2024-11-04 15:08:13 +0800

    feat: initialize codespace

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# clone required repositories
+git clone https://github.com/Azure/azure-cli-extensions.git /workspaces/azure-cli-extensions
+git clone https://github.com/Azure/azure-rest-api-specs.git /workspaces/azure-rest-api-specs
+git clone https://github.com/Azure/aaz.git /workspaces/aaz
+
+# setup development environment
+pip install aaz-dev
+cd /workspaces
+azdev setup -c -r ./azure-cli-extensions
+
+# run codegen tool
+aaz-dev run -c ./azure-cli -e ./azure-cli-extensions -s ./azure-rest-api-specs -a ./aaz
