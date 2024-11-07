6978c61d0aaeba7f46c93844cca145b9e74297d3
necusjz <necusjz@gmail.com>
2024-11-07 14:18:45 +0800

    chore: test

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,9 +6,6 @@ git clone https://github.com/Azure/azure-rest-api-specs.git ./azure-rest-api-spe
 git clone https://github.com/Azure/aaz.git ./aaz
 
 # setup development environment
-#python -m venv ./venv && source ./venv/bin/activate
+source ./venv/bin/activate
 pip install aaz-dev
 azdev setup -c -r ./azure-cli-extensions
-
-# activate venv by default
-echo 'source /workspaces/venv/bin/activate' >> ~/.bashrc
