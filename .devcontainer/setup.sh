76dcc761fd156de0108c8dea6be88d89c2cc14cc
necusjz <necusjz@gmail.com>
2024-11-12 15:37:57 +0800

    feat: use github cli instead

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # clone required repositories
-git clone https://github.com/Azure/azure-cli-extensions.git ./azure-cli-extensions
-git clone https://github.com/Azure/azure-rest-api-specs.git ./azure-rest-api-specs
-git clone https://github.com/Azure/aaz.git ./aaz
+gh repo clone https://github.com/Azure/azure-cli-extensions.git ./azure-cli-extensions
+gh repo clone https://github.com/Azure/azure-rest-api-specs.git ./azure-rest-api-specs
+gh repo clone https://github.com/Azure/aaz.git ./aaz
 
 # setup development environment
 source ./venv/bin/activate
