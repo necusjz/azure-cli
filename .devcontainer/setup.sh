4ddd2e141d3f1092a7464d86447046e48840358e
necusjz <necusjz@gmail.com>
2024-11-18 15:26:15 +0800

    chore: test

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 # define color codes
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-NC='\033[0m'  # no color
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+NC="\033[0m"  # no color
 
 setup_repo() {
     local DIR_NAME="$1"
     local REPO="Azure/$DIR_NAME"
 
     if [ -d "$DIR_NAME" ]; then
-        echo -e "${YELLOW}Pulling the latest changes from upstream...${NC}"
-        cd "$DIR_NAME" || exit
-        gh repo sync --source upstream
+        echo -e "\n${YELLOW}Pulling the latest changes from upstream...${NC}"
+        cd "$DIR_NAME"
+        gh repo sync --source "$REPO"
+        cd /worksapces
     else
-        echo -e "${GREEN}Forking and cloning the repository...${NC}"
+        echo
+        echo -e "\n${GREEN}Forking and cloning the repository...${NC}"
         gh repo fork "$REPO" --clone=true
     fi
 }
@@ -35,6 +37,5 @@ setup_repo "azure-rest-api-specs"
 setup_repo "azure-rest-api-specs-pr"
 
 ELAPSED_TIME=$SECONDS
-echo -e "${YELLOW}Elapsed time: $((ELAPSED_TIME / 60)) min $((ELAPSED_TIME % 60)) sec.${NC}"
-
-echo -e "${GREEN}Finished setup! Please launch the codegen tool via \`aaz-dev run -c azure-cli -e azure-cli-extensions -s azure-rest-api-specs -a aaz\`${NC}"
+echo -e "\n${YELLOW}Elapsed time: $((ELAPSED_TIME / 60)) min $((ELAPSED_TIME % 60)) sec.${NC}"
+echo -e "${GREEN}Finished setup! Please launch the codegen tool via \`aaz-dev run -c azure-cli -e azure-cli-extensions -s azure-rest-api-specs -a aaz\`${NC}\n"
