bd84c6c674340b94ebdf0b6bd7ec7700bdd8f86a
necusjz <necusjz@gmail.com>
2024-11-18 14:54:45 +0800

    refactor: extract a function for setup

--- .devcontainer/login.sh
+++ .devcontainer/login.sh
@@ -1,8 +1,8 @@
 
 export GITHUB_TOKEN=  # logout default account
 
-if gh auth status -a | grep 'Token scopes: ' | grep -q 'repo'; then  # check `repo` scope exists or not
-    echo "You were already logged in to GitHub."
+if gh auth status -a 2>/dev/null | grep 'Token scopes: ' | grep -q 'repo'; then  # check `repo` scope exists or not
+    echo "You were already logged into GitHub."
 else
     gh auth login -p https -w
 fi
--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 
-# fork and clone required repositories
-gh repo fork Azure/aaz --clone=true
-gh repo fork Azure/azure-cli --clone=true
-gh repo fork Azure/azure-cli-extensions --clone=true
-gh repo clone Azure/azure-rest-api-specs
-gh repo clone Azure/azure-rest-api-specs-pr
+# define color codes
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'  # no color
 
-# setup development environment
+setup_repo() {
+    local DIR_NAME="$1"
+    local REPO="Azure/$DIR_NAME"
+
+    if [ -d "$DIR_NAME" ]; then
+        echo -e "${YELLOW}Pulling the latest changes from upstream...${NC}"
+        cd "$DIR_NAME" || exit
+        gh repo sync --source upstream
+    else
+        echo -e "${GREEN}Forking and cloning the repository...${NC}"
+        gh repo fork "$REPO" --clone=true
+    fi
+}
+
+SECONDS=0
+
 pip install aaz-dev
+
+# azdev repositories
+setup_repo "azure-cli"
+setup_repo "azure-cli-extensions"
+
 azdev setup -c -r ./azure-cli-extensions
+
+# aaz-dev repositories
+setup_repo "aaz"
+setup_repo "azure-rest-api-specs"
+setup_repo "azure-rest-api-specs-pr"
+
+ELAPSED_TIME=$SECONDS
+echo -e "${YELLOW}Elapsed time: $((ELAPSED_TIME / 60)) min $((ELAPSED_TIME % 60)) sec.${NC}"
+
+echo -e "${GREEN}Finished setup! Please launch the codegen tool via \`aaz-dev run -c azure-cli -e azure-cli-extensions -s azure-rest-api-specs -a aaz\`${NC}"
